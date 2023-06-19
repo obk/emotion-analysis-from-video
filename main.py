99775165172f56e32b35a728a764c603f6f961cf
obk <bahadir@kundak.dev>
2023-06-19 19:26:36 +0300

    flask

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from flask import Flask, jsonify, request
 import os
 import cv2
 import shutil
@@ -6,85 +7,94 @@ from deepface import DeepFace
 import firebase_admin
 from firebase_admin import credentials, storage
 
+app = Flask(__name__)
+
 cred = credentials.Certificate("./serviceAccountKey.json")
 firebase_admin.initialize_app(cred, {
-  'storageBucket': 'emocall-a5798.appspot.com'
+    'storageBucket': 'emocall-edd87.appspot.com'
 })
 
-bucket = storage.bucket()
+@app.route('/')
+def home():
+    return 'Server is running.'
 
-# List all files in the bucket
-blobs = bucket.list_blobs()
+@app.route('/start', methods=['GET','POST'])
+def start_analysis():
+    result = run_analysis()
+    return jsonify(result)
 
-# Initialize an empty dictionary to store file names and their timestamps
-files = {}
+def run_analysis():
+    try:
+        print('Running analysis...')
 
-for blob in blobs:
-    # Add blob name and timestamp to dictionary
-    files[blob.name] = blob.updated
+        bucket = storage.bucket()
+        blobs = bucket.list_blobs()
+        files = {}
 
-# Find the name of the file that has the latest timestamp
-latest_file = max(files, key=files.get)
+        for blob in blobs:
+            files[blob.name] = blob.updated
 
-# Download the latest file
-blob = bucket.blob(latest_file)
-blob.download_to_filename(f'./{latest_file}')
+        latest_file = max(files, key=files.get)
+        blob = bucket.blob(latest_file)
+        blob.download_to_filename(f'./{latest_file}')
 
-print(f'The latest uploaded file is: {latest_file} and it has been downloaded.')
+        print(f'The latest uploaded file is: {latest_file} and it has been downloaded.')
 
-print(latest_file)
+        cap = cv2.VideoCapture(f'./{latest_file}')
+        fps = cap.get(cv2.CAP_PROP_FPS)
 
-# Load the video
-cap = cv2.VideoCapture(f'./{latest_file}')
+        frame_count = 0
+        emotions = {}
+        emotions_per_second = {}
 
-fps = cap.get(cv2.CAP_PROP_FPS)
+        while True:
+            ret, frame = cap.read()
+            if ret:
+                roi = frame[:frame.shape[0]//2, :]
+                result = DeepFace.extract_faces(roi, enforce_detection=False)
+                if len(result) > 0:
+                    face_coords = result[0]['facial_area']
+                    x, y, w, h = face_coords['x'], face_coords['y'], face_coords['w'], face_coords['h']
+                    face_image = roi[y:y+h, x:x+w]
+                    cv2.imshow("Frame", face_image)
+                    result = DeepFace.analyze(face_image, actions=['emotion'], enforce_detection=False, silent = True)
+                    print("Frame: {}".format(frame_count) + " Emotion: {}".format(result[0]['dominant_emotion']))
+                    emotions[frame_count] = result[0]['dominant_emotion']
+                    current_second = int(frame_count / fps)
+                    if current_second not in emotions_per_second:
+                        emotions_per_second[current_second] = {}
+                    if emotions[frame_count] not in emotions_per_second[current_second]:
+                        emotions_per_second[current_second][emotions[frame_count]] = 0
+                    emotions_per_second[current_second][emotions[frame_count]] += 1
+                    frame_count += 1
+                if cv2.waitKey(1) & 0xFF == ord('q'):
+                    break
+            else:
+                break
 
-frame_count = 0
-emotions = {}  # dictionary to store dominant emotions for each frame
-emotions_per_second = {}  # dictionary to store dominant emotions for each second 
+        result = {}
+        prev_emotion = None
+        emotion_start = 0
 
-while True:
-    ret, frame = cap.read()
-    if ret:
-        # Define the region of interest for cropping - take only top half of the video
-        roi = frame[:frame.shape[0]//2, :]
+        for second in emotions_per_second:
+            dominant_emotion = max(emotions_per_second[second], key=emotions_per_second[second].get)
+            result[str(second)] = {
+                "dominant_emotion": dominant_emotion,
+                "previous_emotion": prev_emotion
+            }
+            if prev_emotion is not None and dominant_emotion != prev_emotion:
+                result[str(second)]["emotion_change_detected"] = True
+                if (second - emotion_start) > 1:
+                    result[str(second)]["emotion_duration"] = second - emotion_start
+                emotion_start = second
+            prev_emotion = dominant_emotion
 
-        # Use deepface to detect faces in the video for each frame
-        result = DeepFace.extract_faces(roi, enforce_detection=False)
-        # Check if a face was detected in the frame
-        if len(result) > 0:
-            # Get the coordinates of the detected face
-            face_coords = result[0]['facial_area']
-            x, y, w, h = face_coords['x'], face_coords['y'], face_coords['w'], face_coords['h']
-            # Crop the face from the original frame
-            face_image = roi[y:y+h, x:x+w]
-            cv2.imshow("Frame", face_image)
-            # Detect face from croped image
-            result = DeepFace.analyze(face_image, actions=['emotion'], enforce_detection=False, silent = True)
-            print("Frame: {}".format(frame_count) + " Emotion: {}".format(result[0]['dominant_emotion']))
-            # Store the dominant emotion for each frame
-            emotions[frame_count] = result[0]['dominant_emotion']
-            # Calculate the current second based on the frame count and FPS
-            current_second = int(frame_count / fps)
-            # Add the dominant emotion to the dictionary for the current second
-            if current_second not in emotions_per_second:
-                emotions_per_second[current_second] = {}
-            if emotions[frame_count] not in emotions_per_second[current_second]:
-                emotions_per_second[current_second][emotions[frame_count]] = 0
-            emotions_per_second[current_second][emotions[frame_count]] += 1
-            frame_count += 1
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
-    else:
-        break
+        cap.release()
+        cv2.destroyAllWindows()
+        return result
 
-prev_emotion = None  # initialize previous emotion as None
-for second in emotions_per_second:
-    dominant_emotion = max(emotions_per_second[second], key=emotions_per_second[second].get)
-    if prev_emotion is not None and dominant_emotion != prev_emotion:
-        print("Emotion changed from {} to {} at second {}".format(prev_emotion, dominant_emotion, second))
-    prev_emotion = dominant_emotion  # update previous emotion for next iteration
+    except Exception as e:
+        return {'error': str(e)}
 
-# Release the VideoCapture object and close any windows
-cap.release()
-cv2.destroyAllWindows()
+if __name__ == '__main__':
+    app.run(port=5000)
